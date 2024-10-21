0132c4319fb81643e55ba3ba38fd63ac4a4a0518
sketchyTK <antkinney@gmail.com>
2024-10-20 21:36:27 -0500

    updating candidate search page

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -2,59 +2,74 @@ import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
 import {Candidate} from '../interfaces/Candidate.interface';
 
-const CandidateSearch = () => {
+const CandidateSearch: React.FC = () => {
 const [candidate, setCandidate] = useState<Candidate | null>(null);
- const [error, setError] = useState<string | null>(null);
-const [username, setUsername] = useState<string>('octocat');
+const [saveCandidates, setSavedCandidates] = useState<Candidate[]>([]);
+const [loading, setLoading] =useState(false);
+const [outOfCandidates, stopCandidates] =useState(false);
+
+  useEffect(() => {
+    fetchRandomCandidate();
+  }, []);
+
+  useEffect(() => {
+    const save = localStorage.getItem('savedCandidates');
+    if (save) {
+      setSavedCandidates(JSON.parse(save));
+    }
+}, []);
 
 
-        const fetchRandomCandidate = async () => {
-            try {
-                const data = await searchGithub();
-                if (data.lengh > 0) {
-                  const randomCandidate = Math.floor(Math.random() * data.length);
-                  
-                setCandidate(data[randomCandidate]);
-                console.log()
-                } else {
-                  setError('No Candidates Found');
-                }
-            } catch (err) {
-                setError('Candidate not found');
-            }
-        };
+  const fetchRandomCandidate = async () => {
+    setLoading(true);
+    const users = await searchGithub();
+    if (users.length === 0) {
+        stopCandidates(true);
+    }
+    else {
+      const user = users[0];
+      const candidateDetails: Candidate = await searchGithubUser(user.login);
+      setCandidate(candidateDetails);
+    }
+    setLoading(false);
+  };
 
-    //   const handleSearch = async () => {
-    //     setError(null);
-    //     try {
-    //         const user = await searchGithubUser(username);
-    //         if (user && user.login) {
-    //             setCandidate(user);
-    //         } else {
-    //             setError('User not found');
-    //         }
-    //     } catch (err) {
-    //         setError('Error fetching user');
-    //     }
-    // };
+const saveCandidateButton = () => {
+  if (candidate) {
+    const updateCandidateList = [...saveCandidates, candidate];
+    setSavedCandidates(updateCandidateList);
+    localStorage.setItem('savedCandidates', JSON.stringify(updateCandidateList));
+  }
+};
 
-    useEffect(() => {
-        fetchRandomCandidate();
-    }, []);
-
-
+const hideCandidateButton = () => {
+  fetchRandomCandidate();
+};
 
   return (
   <div>
     <h1>Candidate Search</h1>
-    {candidate ? (
+    {loading ? (
+          <h2>Loading...</h2>
+      ) : outOfCandidates ? (
+        <h2>No Candidates Available</h2>
+      ) : candidate ? (
             <div>
-                <h2>{candidate.name}</h2>
-                <img src={candidate.avatar_url} alt={candidate.name} width={100}/>
+                <h2>{candidate.name || 'Not Available'}</h2>
+                <img src={candidate.avatar_url} alt={candidate.name} width="200" height="200"/>
+                <p>Location: {candidate.location || 'Not Available'}</p>
+                <p>Email: <a href={`mailto:${candidate.email || 'Not Available'}`}>{candidate.email || 'Not Available'}</a></p>
+                <p>Company: {candidate.company || 'Not Available'}</p>
+                <p>Bio: {candidate.bio || 'Not Available'}</p>
+                <div>
+                  <button onClick={saveCandidateButton}>+</button>
+                  <button onClick={hideCandidateButton}>-</button>
+                </div>
             </div>
             ) : (
-                <h2>Loading...</h2>
-            )}
+              <h2>No Candidate to Display</h2>
+            )
+          }; 
   </div>
   );
   
